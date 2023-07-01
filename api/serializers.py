10fe5a3b5f4d850c6de71504dadb0f645265a34b
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-01 17:10:28 +0500

    to respresantation

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,10 @@ class TaskSerializer(serializers.Serializer):
     description = serializers.CharField(read_only=True)
     completed = serializers.BooleanField(read_only=True, default=False, allow_null=True)
     created_at = serializers.DateTimeField()
+
+    def to_representation(self, instance):
+        return {
+            'pk': instance.id,
+            'sarlavha': instance.title,
+            'description': f'{instance.description[:20]}... at {instance.created_at}'
+        }
